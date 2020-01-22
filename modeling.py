b6256eba1cf1bdca009d5c109837607fa49549b2
Alex Dauenhauer <alexdauenhauer17@gmail.com>
2020-01-22 05:46:47 -0600

    added more dense layers to model

--- modeling.py
+++ modeling.py
@@ -1,3 +1,5 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
 # %%
 import os
 import pickle
@@ -9,12 +11,14 @@ import tensorflow as tf
 from sklearn.utils.class_weight import compute_class_weight
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.layers import (BatchNormalization, ConvLSTM2D, Dense,
-                                     Input, LeakyReLU)
+                                     Input, LeakyReLU, Conv3D)
 from tensorflow.keras.models import Model
 from tensorflow.keras.utils import to_categorical
 from_generator = tf.data.Dataset.from_generator
 
 from DataPrep import DataPrep
+
+
 # %%
 
 # TODO: add variables for frame height and width
@@ -36,9 +40,11 @@ y_test = list(map(lambda x: 0 if x == 'REAL' else 1, y_test))
 y_train = to_categorical(y_train, num_classes=2)
 y_test = to_categorical(y_test, num_classes=2)
 print(len(x_train), len(y_train), len(x_test), len(y_test))
+
+
 # %%
 
-
+# @tf.function
 def input_fn(files, labels, segment_size=5, batch_size=1):
     def dataGenerator():
         for f, label in zip(files, labels):
@@ -63,19 +69,31 @@ def input_fn(files, labels, segment_size=5, batch_size=1):
     )
     dataset = dataset.batch(batch_size)
     return dataset
+
+
 # %%
-batch_size = 5
+batch_size = 10
 train_data = input_fn(x_train, y_train, batch_size=batch_size)
 test_data = input_fn(x_test, y_test, batch_size=batch_size)
+
 
 # %%
 class InputStream(tf.keras.Model):
     def __init__(self, kernel_size, filters, name):
         super().__init__(name=name)
-        self.convLstm = ConvLSTM2D(
+        self.convLstm1 = ConvLSTM2D(
             filters=filters,
-            kernel_size=(kernel_size, kernel_size),
-            strides=(1, 1),
+            kernel_size=kernel_size,
+            strides=1,
+            padding='same',
+            data_format='channels_last',
+            return_sequences=True,
+            dropout=0.5
+        )
+        self.convLstm2 = ConvLSTM2D(
+            filters=filters,
+            kernel_size=kernel_size,
+            strides=1,
             padding='same',
             data_format='channels_last',
             return_sequences=False,
@@ -83,20 +101,78 @@ class InputStream(tf.keras.Model):
         )
         self.bn = BatchNormalization()
         self.act = LeakyReLU()
-        self.dense = Dense(2, name=f'{self.name}_dense')
+        self.flatten = tf.keras.layers.Flatten()
+        self.dense = Dense(512)
+        self.dropout = tf.keras.layers.Dropout(0.5)
+        self.out_layer = Dense(2)
 
     # TODO: specify different behavior for training, i.e. dropout only when
     # training
     def call(self, input_tensor, training=False):
-        x = self.convLstm(input_tensor)
+        x = self.convLstm1(input_tensor)
+        x = self.bn(x)
+        x = self.convLstm1(x)
+        x = self.bn(x)
+        x = self.convLstm2(x)
         x = self.bn(x)
         x = self.act(x)
-        return self.dense(x)
+        x = self.flatten(x)
+        x = self.dense(x)
+        x = self.act(x)
+        x = self.dense(x)
+        x = self.act(x)
+        x = self.dense(x)
+        x = self.act(x)
+        x = self.dropout(x)
+        return self.out_layer(x)
 
 
 # %%
-rgb_stream = InputStream(3, 8, 'rgb_stream')
-flow_stream = InputStream(3, 8, 'flow_stream')
+# class InputStream(tf.keras.Model):
+#     def __init__(self, kernel_size, filters, name):
+#         super().__init__(name=name)
+
+# kernel_size = 3
+# filters = 1
+# rgb_input = tf.keras.Input(shape=(5, 256, 256, 3), name='rgb_input')
+# convLstm1 = ConvLSTM2D(
+#     filters=filters,
+#     kernel_size=kernel_size,
+#     strides=1,
+#     padding='same',
+#     data_format='channels_last',
+#     return_sequences=True,
+#     dropout=0.5
+# )(rgb_input)
+# bn = BatchNormalization()(convLstm1)
+# convLstm2 = ConvLSTM2D(
+#     filters=filters,
+#     kernel_size=kernel_size,
+#     strides=1,
+#     padding='same',
+#     data_format='channels_last',
+#     return_sequences=False,
+#     dropout=0.5
+# )(bn)
+# bn = BatchNormalization()(convLstm2)
+# flatten = tf.keras.layers.Flatten()(bn)
+# dense = Dense(512)(flatten)
+# act = LeakyReLU()(dense)
+# dense = Dense(512)(act)
+# act = LeakyReLU()(dense)
+# dense = Dense(512)(act)
+# act = LeakyReLU()(dense)
+# dropout = tf.keras.layers.Dropout(0.5)(act)
+# out_layer = Dense(2)(dropout)
+# model = Model(inputs=rgb_input, outputs=out_layer)
+# model.summary()
+
+
+# %%
+
+# %%
+rgb_stream = InputStream(3, 32, 'rgb_stream')
+flow_stream = InputStream(3, 32, 'flow_stream')
 rgb_input = tf.keras.Input(shape=(5, 256, 256, 3), name='rgb_input')
 flow_input = tf.keras.Input(shape=(4, 256, 256, 2), name='flow_input')
 rgb = rgb_stream(rgb_input)
@@ -105,10 +181,12 @@ final_average = tf.keras.layers.average([rgb, flow])
 x = tf.keras.layers.Flatten()(final_average)
 final_output = Dense(2, activation='softmax', name='final_output')(x)
 model = Model(
-    inputs={ "rgb_input": rgb_input, "flow_input": flow_input},
+    inputs={"rgb_input": rgb_input, "flow_input": flow_input},
     outputs=final_output,
     name='my_model'
 )
+model.summary()
+
 # %%
 tf.keras.utils.plot_model(
     model,
@@ -116,6 +194,8 @@ tf.keras.utils.plot_model(
     show_shapes=True,
     show_layer_names=True,
 )
+
+
 # %%
 opt = tf.keras.optimizers.Adam()
 model.compile(
@@ -124,15 +204,21 @@ model.compile(
     metrics=['acc'])
 model.fit(
     train_data,
-    epochs=10,
+    epochs=25,
     verbose=1,
     class_weight=class_weights
 )
+
+
 # %%
 model.evaluate(
     test_data,
-    class_weight=class_weights
+    #     class_weight=class_weights
 )
+
+
+# %%
+
 
 # %%
 # DEBUGGING
@@ -144,6 +230,7 @@ for i, x in enumerate(dataset):
     for k, v in data_dict.items():
         print(k, v.numpy().shape)
     print(time.time() - start)
+
 
 # %%
 filepath = 'data/train_sample_videos'
@@ -163,6 +250,7 @@ for i, (f, label) in enumerate(zip(files, labels)):
         print(e)
         break
 print('everything is working now')
+
 
 # %%
 f = 'data/train_sample_videos/adhsbajydo.mp4'
